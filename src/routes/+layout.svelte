e0b1eb1733055265b45b32f414c90a412abcdc2b
grzlz <guillermo.rdzlpz@gmail.com>
2025-06-05 00:55:54 -0600

    Feat: injectAnalytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,12 @@
 	import '../app.css';
 	import { fade } from 'svelte/transition';
 	import { page } from '$app/state';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 	let { children } = $props();
+
+	injectAnalytics();
+
 </script>
 
 <div class="flex flex-col min-h-screen">
