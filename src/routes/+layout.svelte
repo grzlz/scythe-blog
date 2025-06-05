9eaf1c56fd5d80d9c5330f8d9c3ebacb9fd44a5a
grzlz <guillermo.rdzlpz@gmail.com>
2025-06-05 00:12:12 -0600

    Feat: add transition animation between pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script>
-		import BlogHeader from '$lib/components/BlogHeader.svelte';
+	import BlogHeader from '$lib/components/BlogHeader.svelte';
 	import BlogFooter from '$lib/components/BlogFooter.svelte';
-	import '../app.css'; // Import global styles once here
+	import '../app.css';
+	import { fly } from 'svelte/transition';
+	import { page } from '$app/state';
+
 	let { children } = $props();
 </script>
 
 <div class="flex flex-col min-h-screen">
+	<BlogHeader />
 
-		<BlogHeader />
-	{@render children()}
-		<BlogFooter />
+	{#key page.url.pathname}
+		<div in:fly={{ y: 50, duration: 300 }} out:fly={{ y: -50, duration: 300 }}>
+			{@render children?.()}
+		</div>
+	{/key}
+
+	<BlogFooter />
 </div>
