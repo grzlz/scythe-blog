1a250a2b08a9dd300f2804968ce884533530101b
grzlz <guillermo.rdzlpz@gmail.com>
2025-06-09 00:16:30 -0600

    Fix: add z index to navbar to prevent image from ovarlaying

--- src/lib/components/BlogHeader.svelte
+++ src/lib/components/BlogHeader.svelte
@@ -5,7 +5,7 @@
 </script>
 
 
-<nav class="sticky top-0 flex items-center justify-between bg-secondary-800  md:px-8 min-h-[6vh] px-4 py-2">
+<nav class="sticky top-0 flex items-center justify-between bg-secondary-800  md:px-8 min-h-[6vh] px-4 py-2 z-10 shadow-md transition-shadow">
     
     <h1 class="hidden md:block text-primary-200 text-lg font-semibold font-primary">ICARUS</h1>
     
